56b25484e5157a7af384360ae629339142050eb4
Clayton Knapp <csjknapp@gmail.com>
2021-12-13 13:05:21 -0800

    think its working

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-import { getDogs } from './fetch-utils.js';
+import { getDog, getDogs } from './fetch-utils.js';
 import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
 
 // on load
-// fetch all dogs
-// render and append all dog cards to the container
+window.addEventListener('load', async() => {
+    // fetch all dogs
+    const dogsArr = await getDogs();
+    // console.log(dogsArr);
+
+    // render and append all dog cards to the container
+    for (let eachDog of dogsArr) {
+        const dogCardNode = renderDogCard(eachDog);
+        dogListContainer.append(dogCardNode);
+    }
+
+});
